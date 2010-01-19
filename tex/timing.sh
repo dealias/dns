ceb6841fa145cf2e866d1466a66c0b4d32be74cf
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2010-01-19 22:49:39 +0000

    minor

--- tex/timing.sh
+++ tex/timing.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+stop=13
+if [ "$1" != "" ]; then
+    stop=$1
+fi
+
 rm -rf padded unpadded
 touch padded unpadded
+
 echo Timing:
-for i in {6..14}
+for (( i=6; i<=$stop; i++ ))
 do
     echo $i
-    n=$(asy -c "2^$i")
-    echo -e "$n \t $(./a.out $n | grep -A 1 Unpadded | tail -n 1)"| cat >> unpadded
-    n=$(asy -c "ceil(2^$i*2/3)")
-    echo -e "$n \t $(./a.out $n | grep -A 1 Padded | tail -n 1)" | cat >> padded
+    m=$(asy -c "2^$i")
+    echo -e "$m \t $(./a.out $m | grep -A 1 Unpadded | tail -n 1)"| cat >> unpadded
+    m=$(asy -c "ceil(2^$i*2/3)")
+    echo -e "$m \t $(./a.out $m | grep -A 1 Padded | tail -n 1)" | cat >> padded
 done
