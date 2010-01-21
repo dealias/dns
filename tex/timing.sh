d71c386a4c2c247a8ba1424932497803a9d3a41a
John Bowman <bowman@ualberta.ca>
2010-01-21 08:49:50 +0000

    Force m to be odd

--- tex/timing.sh
+++ tex/timing.sh
@@ -14,6 +14,7 @@ do
     echo $i
     m=$(asy -c "2^$i")
     echo -e "$m \t $(./a.out $m 0 | grep -A 1 Unpadded | tail -n 1)"| cat >> unpadded
-    m=$(asy -c "ceil(2^$i*2/3)")
+#    m=$(asy -c "ceil(2^$i*2/3)")
+    m=$(asy -c "int n=ceil(2^$i*2/3); n-1+(n % 2)")
     echo -e "$m \t $(./a.out $m 1 | grep -A 1 Padded | tail -n 1)" | cat >> padded
 done
