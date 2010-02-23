de7b1f72d4e5370d27f078f95588283752e786eb
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2010-02-23 22:26:52 +0000

    added exact test case for cconv

--- tex/cconv.cc
+++ tex/cconv.cc
@@ -2,6 +2,9 @@ using namespace std;
 #include "Complex.h"
 #include "convolution.h"
 
+#define TEST yes
+const double E=2.71828182845904523536;
+const  Complex I(0,1);
 // g++ -g -O3 -DNDEBUG -fomit-frame-pointer -fstrict-aliasing -ffast-math -msse2 -mfpmath=sse cconv.cc fftw++.cc -lfftw3 -march=native
 
 // icpc -O3 -ansi-alias -malign-double -fp-model fast=2 cconv.cc fftw++.cc -lfftw3
@@ -12,8 +15,13 @@ using namespace std;
 // usage: aout [int m] [int pad]
 
 // Number of iterations.
+#ifdef TEST
+unsigned int N=1;
+#else
 unsigned int N=10000000;
+#endif
   
+
 Complex d[]={Complex(-5,3),Complex(3,1),Complex(4,-2),Complex(-3,1),Complex(0,-2),Complex(0,1),Complex(4,0),Complex(-3,-1),Complex(1,2),Complex(2,1),Complex(3,1)};
 
 unsigned int m=sizeof(d)/sizeof(Complex);
@@ -38,8 +46,15 @@ inline void init(Complex *f, Complex *g)
 {
 //  for(unsigned int i=0; i < m; i++) f[i]=d[i];
 //  for(unsigned int i=0; i < m; i++) g[i]=d[i];
+
+#ifdef TEST
+  for(unsigned int i=0; i < m; i++) f[i]=pow(E,m*I);
+  for(unsigned int i=0; i < m; i++) g[i]=pow(E,m*I);
+#else
   for(unsigned int i=0; i < m; i++) f[i]=Complex(3.0,2.0);
   for(unsigned int i=0; i < m; i++) g[i]=Complex(5.0,3.0);
+#endif 
+
 }
 
 int main(int argc, char* argv[])
@@ -168,11 +183,18 @@ int main(int argc, char* argv[])
       for(unsigned int i=0; i < m; i++) cout << h[i] << endl;
     else cout << h[0] << endl;
 
+
     // test accuracy of convolution methods:
     double error=0.0;
-#ifdef TEST    
+#ifdef TEST
+    for(unsigned int i=0; i < m; i++) {
+      // exact solution for test case.
+      h[i]=(i+1)*pow(E,2*m*I);
+      cout << h[i] << endl;
+    }
     for(unsigned int i=0; i < m; i++) 
       error += abs2(h[i]-pseudoh[i]);
+    error /= m;
     cout << "error="<<error<<endl;
     if (error > 1e-12)
       cerr << "Caution! error="<<error<<endl;
