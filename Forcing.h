6c8ebbef344296c38983497327e25dfb6d66ad59
John Bowman <bowman@ualberta.ca>
2011-02-15 18:15:10 +0000

    Remove unused code.

--- Forcing.h
+++ Forcing.h
@@ -4,13 +4,10 @@
 #define FORCING(key) \
 {(void) new Entry<key,ForcingBase>(#key,ForcingTable);}
 
-enum ForcingType {NONE, CONSTANT, STOCHASTIC};
-
 class ForcingBase {
  public:	
   virtual ~ForcingBase() {}
   virtual const char *Name() {return "None";}
-  virtual int Type() {return 0;}
   
   virtual void Set() {}
   virtual void SetStochastic(double dt=0.0) {}
