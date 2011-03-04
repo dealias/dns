50342c19c55cc9764d391464a63474113f57f5d5
John Bowman <bowman@ualberta.ca>
2011-03-04 19:22:47 +0000

    Remove unused variable.

--- dns.cc
+++ dns.cc
@@ -249,7 +249,6 @@ void DNS::InitialConditions()
   mx=(Nx+1)/2;
   my=(Ny+1)/2;
   xorigin=mx-1;
-  origin=xorigin*my;
   nshells=spectrum ? (unsigned) (hypot(mx-1,my-1)+0.5) : 0;
 
   NY[OMEGA]=Nx*my;
