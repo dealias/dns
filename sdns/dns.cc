906c90317343ec6c5359eab521eaab7ea5a58bbe
John Bowman <bowman@ualberta.ca>
2016-06-03 23:48:50 -0600

    Implement 2D advection nonlinearity.

--- sdns/dns.cc
+++ sdns/dns.cc
@@ -2,69 +2,95 @@
 #include "convolution.h"
 #include "Array.h"
 
-// Compile with:
-// g++ -I .. -fopenmp exampleconv2.cc ../convolution.cc ../fftw++.cc -lfftw3 -lfftw3_omp
-
 using namespace std;
 using namespace utils;
 using namespace Array;
 using namespace fftwpp;
 
 // size of problem
-unsigned int m=8;
 
-unsigned int mx=4;
-unsigned int my=4;
+unsigned int Nx=15;
+unsigned int Ny=15;
 
-inline void init(array2<Complex>& f, array2<Complex>& g, unsigned int M=1) 
+int mx;
+unsigned int my;
+
+typedef Array1<Complex>::opt vector;
+typedef Array2<Complex> vector2;
+
+vector2 w;
+vector2 f0,f1,g0,g1;
+
+ImplicitHConvolution2 *Convolution;
+
+void init(vector2& w)
 {
-  unsigned int stop=2*mx-1;
-  unsigned int stopoffset=stop;
-  double factor=1.0/sqrt((double) M);
-  for(unsigned int s=0; s < M; ++s) {
-    double S=sqrt(1.0+s);
-    double ffactor=S*factor;
-    double gfactor=1.0/S*factor;
-    for(unsigned int i=0; i < stop; ++i) {
-      unsigned int I=s*stopoffset+i;
-      for(unsigned int j=0; j < my; j++) {
-        f[I][j]=ffactor*Complex(i,j);
-        g[I][j]=gfactor*Complex(2*i,j+1);
-      }
+  for(int i=-mx+1; i < mx; ++i) {
+    for(unsigned j=(i <= 0 ? 1 : 0); j < my; ++j) {
+      w[i][j]=Complex(i,j);
     }
   }
+}
+    
+void Source(const vector2& w, vector2 &S)
+{
+  f0[0][0]=0.0;
+  f1[0][0]=0.0;
+  g0[0][0]=0.0;
+  g1[0][0]=0.0;
+  
+  for(int i=-mx+1; i < mx; ++i) {
+    for(unsigned j=(i <= 0 ? 1 : 0); j < my; ++j) {
+      Real k2=i*i+j*j;
+      Complex kxw=Complex(-w[i][j].im*i,w[i][j].re*i);
+      Complex kyw=Complex(-w[i][j].im*j,w[i][j].re*j);
+      f0[i][j]=kxw;
+      f1[i][j]=kyw;
+      double k2inv=1.0/k2;
+      g0[i][j]=kyw*k2inv;
+      g1[i][j]=-kxw*k2inv;
+    }
+  }
+
+  Complex *F[]={f0,f1,g0,g1};
+  Convolution->convolve(F,multbinary2);
+  f0[0][0]=0.0;
+  
+  fftwpp::HermitianSymmetrizeX(mx,my,mx-1,f0);
+  
+#if 1
+  Real sum=0.0;
+  for(int i=-mx+1; i < mx; ++i) {
+    for(unsigned j=(i <= 0 ? 1 : 0); j < my; ++j)
+      sum += (f0[i][j]*conj(w[i][j])/(i*i+j*j)).re;
+  }
+  cout << sum << endl;
+  cout << endl;
+#endif
 }
 
 int main(int argc, char* argv[])
 {
-  fftw::maxthreads=get_max_threads();
-
-#ifndef __SSE2__
-  fftw::effort |= FFTW_NO_SIMD;
-#endif  
-  
-  cout << "2D centered Hermitian-symmetric convolution:" << endl;
-
+  mx=(Nx+1)/2;
+  my=(Ny+1)/2;
+    
   size_t align=sizeof(Complex);
-  array2<Complex> f(2*mx-1,my,align);
-  array2<Complex> g(2*mx-1,my,align);
-  init(f,g);
-  cout << "\ninput:" << endl;
-  cout << "f:" << endl << f;
-  cout << "g:" << endl << g;
   
-  /*
-    cout << "input after symmetrization (done automatically):" << endl;
-    HermitianSymmetrizeX(mx,my,mx-1,f);
-    HermitianSymmetrizeX(mx,my,mx-1,g);
-    cout << "f:" << endl << f << endl;
-    cout << "g:" << endl << g << endl;
-  */
-  
-  bool symmetrize=true;
-  ImplicitHConvolution2 C(mx,my);
-  C.convolve(f,g,symmetrize);
-  cout << "\noutput:" << endl << f;
+  f0.Allocate(Nx,my,-mx+1,0,align);
+  f1.Allocate(Nx,my,-mx+1,0,align);
+  g0.Allocate(Nx,my,-mx+1,0,align);
+  g1.Allocate(Nx,my,-mx+1,0,align);
 
+  Convolution=new fftwpp::ImplicitHConvolution2(mx,my,true,true,4,1);
+  
+  w.Allocate(Nx,my,-mx+1,0,align);
+  
+  init(w);
+  
+  w(0,0)=0;
+  fftwpp::HermitianSymmetrizeX(mx,my,mx-1,w);
+
+  Source(w,f0);
+    
   return 0;
 }
