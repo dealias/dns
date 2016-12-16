dealias/dns

commit 3d15f8bb5eaf31dc6c53dd35bdd88983052263eb

Output stochastic forcing amplitude.

2d/dns.cc
@@ -214,6 +214,13 @@ public:
     return true;
   }
   
+  // For forcing diagnostic
+  void Force(Complex& w, Complex& S, double&, int i, int j) {
+    if(active(i,j)) {
+      S=sqrt(2.0*eta*etanorm);
+    }
+  }
+  
   void ForceStochastic(Complex& w, double& T, int i, int j) {
      if(active(i,j)) {
       T += realproduct(f,w)+0.5*abs2(f);

2d/zve.asy
@@ -20,7 +20,7 @@ void fnorm() {
     int i=Fi[k];
     int j=Fj[k];
     real f=F[k];
-    l += f;                         // l   := |A^(1/2)f|
+    l += f*f;                         // l   := |A^(1/2)f|
     sum += f*f/(i*i+j*j);           // sum := |f|^2    
   }
   g= sqrt(sum);                     // g   := |f|
@@ -49,7 +49,7 @@ while(nextrun()) {
   draw(graph(new real(real E) {return E;},0,1.0), blue); //point(plain.E).x),blue);
   draw(graph(new real(real E) {return kfp^2*E;},
              0,min(point(plain.N).y/kfp^2,point(plain.E).x)),magenta);
-  draw(graph(new real(real E) {return sqrt(E);},0,(point(plain.N).y)^2),red);
+  //  draw(graph(new real(real E) {return sqrt(E);},0,(point(plain.N).y)^2),red);
 
   ++k;
 }

commit 0d8c8c42436a71194fc0dd433f3369e98ccf3285

Fix white-noise forcing bug introduced in 3d15f8bb5eaf31dc6c53dd35bdd88983052263eb.

2d/Forcing.h
@@ -14,6 +14,10 @@ class ForcingBase {
   virtual bool active(int i, int j) {return false;} 
   virtual bool Stochastic(double dt=0.0) {return false;}
   virtual void Force(Complex& w, Complex& S, double& T, int i, int j) {}
+  virtual void ForceMask(Complex& w, Complex& S, double& T, int i, int j) 
+  {
+    Force(w,S,T,i,j);
+  }
   virtual void ForceStochastic(Complex& w, double& T, int i, int j) {}
 };
 

2d/dns.cc
@@ -80,7 +80,7 @@ public:
       Complex w=0.0;
       double T=0.0;
       Complex S=0.0;
-      Forcing->Force(w,S,T,i,j);
+      Forcing->ForceMask(w,S,T,i,j);
       if(S != 0.0)
         b->fprolog << i << j << abs(S);
     }
@@ -215,7 +215,7 @@ public:
   }
   
   // For forcing diagnostic
-  void Force(Complex& w, Complex& S, double&, int i, int j) {
+  void ForceMask(Complex& w, Complex& S, double&, int i, int j) {
     if(active(i,j)) {
       S=sqrt(2.0*eta*etanorm);
     }