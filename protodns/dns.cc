120fe5950c8bff7464f3a25925702694f9822923
John Bowman <bowman@ualberta.ca>
2020-07-02 17:54:03 -0600

    Implement multSmagorinsky2.

--- protodns/dns.cc
+++ protodns/dns.cc
@@ -42,12 +42,75 @@ void init(vector2& w)
   }
 }
 
+#ifdef __SSE2__
+static inline Vec sqrt(const Vec& z)
+{
+  return _mm_sqrt_pd(z);
+}
+#else
+static inline Vec sqrt(const Vec& z)
+{
+  return Vec(sqrt(z.x),sqrt(z.y));
+}
+#endif
+
+// 2D Navier-Stokes advection a la Basdevant with Smagorinsky subgrid model
+// requiring only 4 inputs and 2 outputs.
+double Cd;
+
+void multSmagorinsky2(double **F, unsigned int m,
+                    const unsigned int indexsize,
+                    const unsigned int *index,
+                    unsigned int r, unsigned int threads)
+{
+  double* F0=F[0];
+  double* F1=F[1];
+  double* F2=F[2];
+  double* F3=F[3];
+
+#ifdef __SSE2__
+  unsigned int m1=m-1;
+  PARALLEL(
+    for(unsigned int j=0; j < m1; j += 2) {
+      double *F0j=F0+j;
+      double *F1j=F1+j;
+      double *F2j=F2+j;
+      double *F3j=F3+j;
+      Vec u=LOAD(F0j);
+      Vec v=LOAD(F1j);
+      Vec ux=LOAD(F2j);
+      Vec s12=LOAD(F3j);
+      STORE(F0j,v*v-u*u+4*Cd*Cd*sqrt(ux*ux+s12*s12)*ux); // B(x,t)
+      STORE(F1j,u*v);
+    }
+    );
+  if(m % 2) {
+    double u=F0[m1];
+    double v=F1[m1];
+    double ux=F2[m1];
+    double s12=F3[m1];
+    F0[m1]=v*v-u*u+4*Cd*Cd*sqrt(ux*ux+s12*s12)*ux; // B(x,t)
+    F1[m1]=u*v;
+  }
+#else
+  for(unsigned int j=0; j < m; ++j) {
+    double u=F0[j];
+    double v=F1[j];
+    double ux=F2[j];
+    double s12=F3[j];
+    F0[j]=v*v-u*u+4*Cd*Cd*sqrt(ux*ux+s12*s12)*ux; // B(x,t)
+    F1[j]=u*v;
+  }
+#endif
+}
+
 void Source(const vector2& w, vector2 &S)
 {
   f0[0][0]=0.0; // Enforce no mean flow.
   f1[0][0]=0.0;
-  fft2d Forward(Nx,my,-1);
-  fft2d Backward(Nx,my,1);
+  f2[0][0]=0.0;
+  f3[0][0]=0.0;
+
   // This 2D version requires only 6 FFTs per stage (in the spirit
   // of Basdevant, J. Comp. Phys, 50, 1983).
   for(int i=-mx+1; i < mx; ++i) {
@@ -56,36 +119,21 @@ void Source(const vector2& w, vector2 &S)
       double jk2inv=j*k2inv;
       double ik2inv=i*k2inv;
       Complex wij=w[i][j];
-      double u=Complex(-wij.im*jk2inv,wij.re*jk2inv);
-      double v=Complex(wij.im*ik2inv,-wij.re*ik2inv);
+      Complex u=Complex(-wij.im*jk2inv,wij.re*jk2inv);
+      Complex v=Complex(wij.im*ik2inv,-wij.re*ik2inv);
       f0[i][j]=u;
       f1[i][j]=v;
       f2[i][j]=Complex(-i*u.im,i*u.re); // dudx
       f3[i][j]=Complex(-i*v.im,i*v.re)+Complex(-j*u.im,j*u.re); // dvdx + dudy
     }
   }
-  Backward.fftNormalized(f0);
-  Backward.fftNormalized(f1);
-  Backward.fftNormalized(f2);
-  Backward.fftNormalized(f3);
-  for(int i=-mx+1; i < mx; ++i) {
-    for(int j=(i <= 0 ? 1 : 0); j < my; ++j) {
-      double v=f1[i][j];
-      double u=f0[i][j];
-      double C=Cs*delta;
-      double ux=f2[i][j];
-      double s12=f3[i][j];
-      f1[i][j]=v*v-u*u+4*C*C*sqrt(ux*ux+s12*s12)*ux; // B(x,t)
-      f0[i][j]=u*v;
-    }
-  }
-
-  Forward.fft(f0);
-  Forward.fft(f1);
+q
+  Complex *F[]={f0,f1,f2,f3};
+  Convolution->convolve(F,multSmagorinsky2);
 
   for(int i=-mx+1; i < mx; ++i) {
     for(int j=(i <= 0 ? 1 : 0); j < my; ++j) {
-      f0[i][j]=(i*i-j*j)*f0[i][j]+i*j*f1[i][j]-nu*(i*i+j*j)*w[i][j];
+      f0[i][j]=i*j*f0[i][j]+(i*i-j*j)*f1[i][j]-nu*(i*i+j*j)*w[i][j];
     }
   }
 }
@@ -148,6 +196,8 @@ int main(int argc, char* argv[])
   f3.Allocate(Nx,my,-mx+1,0,align);
 
   Convolution=new ImplicitHConvolution2(mx,my,true,true,4,2);
+
+  Cd=Cs*delta;
 
   w.Allocate(Nx,my,-mx+1,0,align);
 
